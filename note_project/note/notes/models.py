7b886b7141f88ad4f8a3f5cb51fee1d21b84df27
Melanie Pena <penamelanie3@gmail.com>
2017-04-30 01:18:59 +0000

    added forms.py

--- note_project/note/notes/models.py
+++ note_project/note/notes/models.py
@@ -10,12 +10,11 @@ class Note(models.Model):
     body = models.TextField()
     pubTime = models.DateTimeField(auto_now_add = True)
     tags = models.ManyToManyField('Tag', related_name= 'notes')
-    
     def __unicode__(self):
         return self.label
     
+
 class Tag(models.Model):
     label = models.CharField(max_length=200)
-    
     def __unicode__(self):
         return self.label
