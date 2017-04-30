914a3e2477ff228881d63eb445b1d27f916d3885
Melanie Pena <penamelanie3@gmail.com>
2017-04-30 19:28:23 +0000

    added add_note view and its html

--- note_project/note/notes/views.py
+++ note_project/note/notes/views.py
@@ -24,6 +24,16 @@ def home_view(request):
 def index_view(request):
     notes = Note.objects.all()
     return render(request, 'notes/index.html',{'notes':notes})
-    
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messaegs.INFO, 'Note Added')
+            return HttpResponseRedirect(reverse('notes:index'))
+    else:
+        form = NoteForm()
+    return render(request, 'notes/addnote.html', {'form': form})
     
     
