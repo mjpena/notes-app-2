3b5cdb7c854f0303ce8c6c00889f67df50479118
Melanie Pena <penamelanie3@gmail.com>
2017-04-30 03:30:30 +0000

    added login authentication to homepage

--- note_project/note/notes/views.py
+++ note_project/note/notes/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import athenticate, login
+from django.http import HttpReponseRedirect
 
 # Create your views here.
 
+def index_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        
+        auth = authenticate(username = username, password=password)
+        if auth is not None:
+            login(request, auth)
+            return HttpResponseRedirect(reverse('notes:index'))
+        else:
+            messages.add_messaeg(request, messages.INFO, "Authentication Failed")
+            return HttpResponseRedirect(reverse('home'))
+    
+    return render(request, 'home.html')
+    
+    
